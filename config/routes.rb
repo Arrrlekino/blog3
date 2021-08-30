9d705dbf72d051e68214fccf7e4fb06bac07dcfb
arrrlekino <zebrasteel@gmail.com>
2021-08-30 11:18:48 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'posts/index'
   root to: "posts#index"
+  get 'posts/all', to: 'posts#all'
+  get 'posts/:id', to: 'posts#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
