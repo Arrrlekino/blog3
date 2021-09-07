b5625325b067934dffe614a463805e8db83bbb95
arrrlekino <zebrasteel@gmail.com>
2021-09-07 23:00:49 +0200

    add controller/book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,12 +7,17 @@ class BooksController < ApplicationController
   def new
     @topics = Topic.all()
     @authors = Author.all()
+    @book = Book.new
   end
 
   def create
-    @book = Book.new(params.require(:book).permit(:title, :topic_id, :authorship_id, :cover_url))
+    @book = Book.new(book_params)
+   # @book = Book.new(params.require(:book).permit(:title, :topic_id, :authorship_id[author_id], :cover_url))
     @book.save
 
     redirect_to '/books/' + @book.id.to_s
+  end
+  def book_params
+    params.require(:book).permit(:title, :topic_id, :cover_url, authorship_id: [])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   root to: "books#index"
   get 'posts/all', to: 'posts#all'
-  get 'posts/:id', to: 'posts#show'
+  #get 'posts/:id', to: 'posts#show'
+  get 'books/:id', to: 'books#show'
 
 
 
